1c90cd2e514d837944728720ffa27341240e76ab
MARISTIKA GINTING <maristikagtg@gmail.com>
2018-11-13 10:36:02 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>e-commerce about</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>e-commerce</title>
+</head>
+<body>
+    <th>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+    </th>
+</body>
+</html>
