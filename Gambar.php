0ae61e0404425cf121fc22aded372547151f6cc9
maristikaginting <44989632+maristikaginting@users.noreply.github.com>
2018-11-20 23:08:23 +0700

    Update Gambar.php

--- Gambar.php
+++ Gambar.php
@@ -1,1 +1,19 @@
+<html>
+<head>
+<title>image</title>
+</head>
 
+<body background="pink">
+
+
+<marquee direction=“left”><img src="kamar.jpg" width=350 height=300 title="kamar" 
+hspace=15 vspace=20 align=middle></marquee>
+
+<marquee direction =right loop = 10><img src="Kamar1.jpg" width= 350 height=300 title="kamar1" 
+hspace=15 vspace=20 align=middle></marquee>
+
+<marquee direction=“left”><img src="kamar2.jpg" width=350 height=300 title="kamar2" 
+hspace=15 vspace=20 align=middle></marquee>
+
+</body>
+</html>
