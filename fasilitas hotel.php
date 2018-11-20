2f7aa0db161ee002d109f5423d2a65091816fdd6
maristikaginting <44989632+maristikaginting@users.noreply.github.com>
2018-11-20 22:33:12 +0700

    Update fasilitas hotel.php

--- fasilitas hotel.php
+++ fasilitas hotel.php
@@ -1,1 +1,74 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>e-commerce</title>
+</head>
+<body>
+<h1 class="brand">GALERI & FASILITAS HOTEL</h1>
+    <p>
+    	<br /><hr class="line" />
+    </p>
+    <p>&nbsp;</p>
+    <div class="left" style="margin-right:10px; width:300px;">
+    	<img src="images/restoran.jpg" width="300" height="200" /> 
+        <div class="text--img">Restorant</div>
+        <div class="artikel--img">
+        <p>
+   	    <strong>Restoran</strong> adalah istilah umum untuk menyebut   usaha gastronomi yang menyajikan hidangan 
+        kepada masyarakat kepada   masyarakat dan menyediakan tempat untuk menikmati hidangan itu serta   menetapkan tarif 
+        tertentu untuk makanan dan pelayanannya. </p></div>
+    </div>
+    <div class="left" style="margin-right:10px; width:300px;">
+    	<img src="images/karaoke-room.jpg" width="300" height="200" /> 
+        <div class="text--img">Karaoke Rooms</div>
+        <div class="artikel--img">
+   	      <p><strong>Karaoke</strong> adalah bentuk hiburan   interaktif atau video game di mana penyanyi amatir bernyanyi bersama   
+          dengan rekaman musik (a music video ) dengan menggunakan mikrofon dan   sound sistem publik. </p>
+   	      <p>Musik karaoke biasanya lagu minus lead vocal.   
+          Lyrics biasanya ditampilkan pada layar video, bersama dengan simbol   bergerak, berubah warna, atau gambar video musik, 
+          untuk membimbing   penyanyi.        </p>
+        </div>
+    </div>
+    <div class="left" style="width:300px;">
+    	<img src="images/spa.jpg" width="300" height="200" /> 
+        <div class="text--img">SPA Center</div>
+        <div class="artikel--img">
+          <p><strong>Spa</strong> menawarkan perawatan tubuh dengan metode relaksasi. Perawatan ini   memang terbilang cukup mahal. 
+          Namun, hasilnya sebanding dengan harga   yang harus kita bayarkan. </p>
+          <p>Anda bisa memanjakan tubuh sambil menikmati   pijatan lembut dari terapis handal. 
+          Berikut adalah manfaat dari   perawatan tubuh di spa, seperti dilansir <em>Boldsky</em>.</p>
+        </div>
+    </div>
+    <div class="left" style="margin-right:10px; width:300px;">
+    	<img src="images/fitness.jpg" width="300" height="200" /> 
+        <div class="text--img">Fitness Center</div>
+        <div class="artikel--img">
+        <p>
+   	  	<strong>Fitness</strong> 
+      	merupakan salah satu kegiatan melatih tubuh yang kini menjadi makin   nge-trend di kalangan anak muda. 
+      	Tak hanya dapat membentuk tubuh yang   bugar dan sehat, ternyata masih banyak sekali manfaat lainnya dari   fitness. </p></div>
+    </div>
+    <div class="left" style="margin-right:10px; width:300px;">
+    	<img src="images/kolam renang.jpg" width="300" height="200" /> 
+        <div class="text--img">Poll</div>
+        <div class="artikel--img">
+       	  <p><strong>Kolam renang</strong> adalah suatu konstruksi buatan yang dirancang untuk diisi dengan air dan 
+          digunakan untuk berenang, menyelam, atau aktivitas air lainnya. </p>
+       	  <p>Kolam renang umum biasanya adalah bagian dari pusat kebugaran jasmani atau taman rekreasi, 
+          dengan fasilitas-fasilitas lainnya meliputi sauna, lapangan olahraga renang (squash, tenis, dll) dan rumah makan. 
+          Untuk menjernihkan dan mendisfeksi air biasanya digunakan kaporit. </p>
+      </div>
+    </div>
+    <div class="left" style="width:300px;">
+    	<img src="images/resepsionis.jpg" width="300" height="200" /> 
+        <div class="text--img">Restorant</div>
+        <div class="artikel--img">
+        	<p>
+   	    <strong>Restoran</strong> adalah istilah umum untuk menyebut   usaha gastronomi yang menyajikan hidangan 
+        kepada masyarakat kepada   masyarakat dan menyediakan tempat untuk menikmati hidangan itu serta   menetapkan tarif 
+        tertentu untuk makanan dan pelayanannya. </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
