a86acc0b1bd0fab7e0140193beedfd558fee9d34
maristikaginting <44989632+maristikaginting@users.noreply.github.com>
2018-11-20 20:52:55 +0700

    Create data.php

---
+++ data.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>e-commerce</title>
+</head>
+<body>
+    <th>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <p>data</p>
+        <p>kisah</p>
+    </th>
+</body>
+</html>
